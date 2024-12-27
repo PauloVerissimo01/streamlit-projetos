739161009dfa094667c5756def4dcb765d358ed6
Paulo Verisismo <paulosousaverissimo@gmail.com>
2024-12-27 17:15:20 -0400

    3° commit - Atualizando o Mostrar Tabela n o app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import streamlit as st # nos permite criar uma aplocação web, para nosso projeto de Dados
 
 
+# função para selecionar a quantidade de linhas do dataframe
+def mostra_qntd_linhas(dataframe):
+    qntd_linhas = st.sidebar.slider('Selecione a quantidade de linhas que deseja mostrar na tabela', min_value=1,max_value = len(dataframe),step = 1)
+    st.write(dataframe.head(qntd_linhas).style.format(subset = ['Valor'], formatter="{:.2f}"))
+
 # importando os dados
 
 dados = pd.read_csv('estoque.csv')
@@ -19,11 +24,18 @@ checkbox_mostrar_tabela = st.sidebar.checkbox('Mostrar tabela')
 
 
 
+
 if checkbox_mostrar_tabela:
 
     st.sidebar.markdown('Filtro para tabela')
-
-    categorias = list[dados['Categoria'].unique()]
+                        #Akie ele está pegaando os valores unicos da coluna Categoria
+    categorias = list(dados['Categoria'].unique())
     categorias.append('Todas')
+    categoria = st.sidebar.selectbox('Selecione a categoria para apresentar na tabela', options = categorias)
 
- 
+
+    if categoria != 'Todas':
+        df_categoria = dados.query('Categoria ==@categoria')
+        mostra_qntd_linhas(df_categoria)       
+    else:
+        mostra_qntd_linhas(dados)       
